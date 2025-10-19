95b8de7be8f9c8f92f59cca7d5c31370a3a26df5
Lattuse <sultik280805@gmail.com>
2025-10-19 22:16:54 +0500

    mainN

--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,10 @@ import facade.RealEstateFacade;
 public class Main {
     public static void main(String[] args) {
         RealEstateFacade facade = new RealEstateFacade();
-        String buyer = "Iosiph Stalin";
+        String buyerName = "Iosiph Stalin";
         String propertyName = "Luxury Dacha";
         double amount = 250000;
-        facade.buyProperty(buyer, propertyName, amount);
+        facade.buyProperty(buyerName, propertyName, amount);
     }
 }
 
